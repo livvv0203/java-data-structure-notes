aa6773570c4ace7374f8b9b5dd8946be31012e6d
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2022-08-27 13:41:29 -0700

    TextBox class

--- src/com/jieqing/Main.java
+++ src/com/jieqing/Main.java
@@ -3,9 +3,11 @@ package com.jieqing;
 public class Main {
 
     public static void main(String[] args) {
-
+        TextBox textBox1 = new TextBox(); // Referencing the address on heap
+        TextBox textBox2 = textBox1;
+        textBox1.setText(("Box 1"));
+        System.out.println(textBox1.text.toUpperCase());
     }
-
 }
 
 
---
+++ src/com/jieqing/TextBox.java
@@ -0,0 +1,15 @@
+package com.jieqing;
+
+public class TextBox {
+
+    public String text = ""; // Reference type
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void clear() {
+        this.text = "";
+    }
+
+}
