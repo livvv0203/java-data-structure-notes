e54ac01ad391dffa272ad7e59da437c46d40fce5
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2022-08-28 16:13:48 -0700

    Linked List Implementation, Add/Remove, size(), IndexOf, etc

---
+++ src/com/jieqing/LinkedList.java
@@ -0,0 +1,141 @@
+package com.jieqing;
+
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+    /**
+     * Node Class declared inside the LinkedList class
+     */
+    private class Node {
+        private int value;
+        private Node next; // Reference to the next Node
+        // Construct a Node with value
+        // This is a custom constructor
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node first; // Head Node
+    private Node last; // Tail Node
+    private int size;
+
+    public void addLast(int item) {
+        Node node = new Node(item);
+
+        if (isEmpty()) {
+            first = last = node;
+        } else {
+            last.next = node; // linking last node to the new node
+            last = node; // Update last to point to the new node
+        }
+
+        size++;
+    }
+
+    public void addFirst(int item) {
+        Node node = new Node(item);
+
+        if (isEmpty()) {
+            first = last = node;
+        } else {
+            node.next = first; // link
+            first = node; // update
+        }
+
+        size++;
+    }
+
+    public void removeFirst() {
+        // [10 -> 20 -> 30]
+        // Link list is empty
+        if (isEmpty())
+            throw new NoSuchElementException();
+        // Only one node
+        if (first == last) {
+            first = last = null;
+            return;
+        }
+        // At least 2 nodes
+        Node second = first.next; // point to 20, as a backup
+        first.next = null; // this will remove the link between 10 and 20
+        first = second; // update first
+
+        size--;
+    }
+
+    public void removeLast() {
+        // List is empty
+        if (isEmpty())
+            throw new NoSuchElementException();
+        // Only 1 node, there's no prev exist
+        if (first == last) {
+            first = last = null;
+        } else {
+            // At least 2 nodes
+            // [10 -> 20 -> 30]
+            // previous -> 20, last -> 30
+            Node previous = getPrevious(last);
+            // Make last to point previous
+            last = previous; // this will shrink list
+            last.next = null; // remove the link
+        }
+        size--;
+    }
+
+    private Node getPrevious(Node node) {
+        Node current = first;
+        while(current != null) {
+            // Found the prev node
+            if (current.next == node) return current;
+            current = current.next;
+        }
+        return null;
+    }
+
+    public int indexOf(int item) {
+        int index = 0;
+        Node currentNode = first;
+        // As traversing the list, increment index
+        while(currentNode != null) {
+            if (currentNode.value == item) return index;
+            // otherwise,
+            currentNode = currentNode.next;
+            index++;
+        }
+        return -1;
+    }
+    // O(1)
+    public int size() {
+        return size;
+    }
+
+    public int[] toArray() {
+        int[] array = new int[size];
+        // Traverse the list, add each node to array
+        Node current = first;
+        int index = 0;
+        while(current != null) {
+            array[index] = current.value;
+            index++;
+            current = current.next; // set current to reference the next node
+        }
+        return array;
+    }
+
+    public boolean contains(int item) {
+        return indexOf(item) != -1;
+    }
+
+    private boolean isEmpty() {
+        return first == null;
+    }
+}
+
+
+
+
+
+
+
+
--- src/com/jieqing/Main.java
+++ src/com/jieqing/Main.java
@@ -1,12 +1,18 @@
 package com.jieqing;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        LinkedList list = new LinkedList();
+        list.addLast(20);
+        list.addLast(30);
+        list.addFirst(10);
 
-
-
+        int[] array = list.toArray();
+        System.out.println(Arrays.toString(array));
 
     }
 }
