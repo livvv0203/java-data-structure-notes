3ce5e4d6265d11b6613cd13d428d22feba1cded6
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2022-09-01 01:34:07 -0700

    Hash Map Demo

--- src/com/jieqing/Customer.java
+++ src/com/jieqing/Customer.java
@@ -3,11 +3,12 @@ package com.jieqing;
 public class Customer {
 
     private String name;
+    private String email;
 
-    public Customer(String name) {
+    public Customer(String name, String email) {
         this.name = name;
+        this.email= email;
     }
-
 
     public String getName() {
         return name;
@@ -15,5 +16,18 @@ public class Customer {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
--- src/com/jieqing/MapDemo.java
+++ src/com/jieqing/MapDemo.java
@@ -1,16 +1,38 @@
 package com.jieqing;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapDemo {
 
     public static void show() {
-        List<Customer> customers = new ArrayList<>();
-        for (Customer customer : customers) {
-            if (customer.getName() == "John") {
-                System.out.println("Found!");
-            }
+
+        Customer c1 = new Customer("a", "e1");
+        Customer c2 = new Customer("b", "e2");
+
+        Map<String, Customer> map = new HashMap<>();
+        map.put(c1.getEmail(), c1);
+        map.put(c2.getEmail(), c2);
+
+        Customer unknown = new Customer("Unknown", "N/A");
+        Customer customer = map.get("e1"); // O(1)
+        Customer customer1 = map.getOrDefault("e10", unknown);
+
+        boolean isExist = map.containsKey("e1");
+
+        for (Map.Entry e : map.entrySet()) {
+            System.out.println(e.getValue());
         }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
